36d8363566a4c8f289b24e812726a1978e26ddb8
Louis hoàng <thang.uha236@gmail.com>
2024-08-14 15:17:55 +0700

    comple show data in table

--- TodoList_App/ViewController.swift
+++ TodoList_App/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = ["thanwg", "hoang"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "List"
+        
+        setupTableView()
+        
     }
-
-
+    
+    
+    @IBAction func didTapAddButton(_ sender: Any) {
+        guard let vc = storyboard?.instantiateViewController(identifier: "entry") as? EntryViewController else{
+            fatalError()
+        }
+        vc.title = "New Task"
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
+    func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
 }
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+        
+    }
+    
+}
+
 
---
+++ TodoList_App/ViewController/EntryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EntryViewController.swift
+//  TodoList_App
+//
+//  Created by Louis Macbook on 14/08/2024.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
