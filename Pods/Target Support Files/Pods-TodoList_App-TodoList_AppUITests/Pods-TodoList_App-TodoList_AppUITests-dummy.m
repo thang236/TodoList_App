b81a6a31057b54b711a7b01897a85de46711d330
Louis hoàng <thang.uha236@gmail.com>
2024-08-19 16:21:31 +0700

    remove pods

--- Pods/Target Support Files/Pods-TodoList_App-TodoList_AppUITests/Pods-TodoList_App-TodoList_AppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TodoList_App_TodoList_AppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TodoList_App_TodoList_AppUITests
-@end
--- Pods/Target Support Files/Pods-TodoList_AppTests/Pods-TodoList_AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TodoList_AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_TodoList_AppTests
-@end
