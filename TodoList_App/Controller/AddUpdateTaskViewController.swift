08d53fd559bc52351726f94d248213f8dec0cee0
Louis hoàng <thang.uha236@gmail.com>
2024-08-29 17:16:23 +0700

    Feature updateTask -  Add update functionality with reused layout from add screen -  Fix date, time picker - change name AddViewController -> AddUpdateViewController

--- TodoList_App/Controller/AddUpdateTaskViewController.swift
+++ TodoList_App/Controller/AddUpdateTaskViewController.swift
@@ -1,5 +1,5 @@
 //
-//  AddTaskViewController.swift
+//  AddUpdateTaskViewController.swift
 //  TodoList_App
 //
 //  Created by Louis Macbook on 27/08/2024.
@@ -8,10 +8,11 @@
 import UIKit
 
 protocol AddTaskViewControllerDelegate {
-    func didCreateTask(newTask: TaskModel)
+    func didCreateTask()
+    func didUpdateTask()
 }
 
-class AddTaskViewController: UIViewController {
+class AddUpdateTaskViewController: UIViewController {
     var delegate: AddTaskViewControllerDelegate?
     @IBOutlet private var descriptionTextField: UITextView!
     @IBOutlet private var timeTextField: UITextField!
@@ -23,100 +24,122 @@ class AddTaskViewController: UIViewController {
     @IBOutlet private var importantButton: UIButton!
     @IBOutlet private var personButton: UIButton!
     @IBOutlet private var groupButton: UIButton!
-    
+    @IBOutlet private var headerLabel: UILabel!
+
     private let currentDate = Date()
     private var isImportant: Bool = false
-    private var isPerson: Bool = true
+    private var isGroup: Bool = false
     private let datePicker = UIDatePicker()
     private let timePicker = UIDatePicker()
-    
+
     private let taskService: TaskService
-    init(taskService: TaskService) {
+    private let dataEdit: TaskModel?
+    init(taskService: TaskService, dataEdit: TaskModel?) {
         self.taskService = taskService
+        self.dataEdit = dataEdit
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init\(coder) has not been implemented")
     }
-    
-    static func create() -> AddTaskViewController {
+
+    static func create(taskEdit: TaskModel?) -> AddUpdateTaskViewController {
         let taskService = TaskServiceImpl()
-        let addTaskVC = AddTaskViewController(taskService: taskService)
-        
+        let addTaskVC = AddUpdateTaskViewController(taskService: taskService, dataEdit: taskEdit)
         return addTaskVC
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTexField()
         setupLayout()
         showTimePicker()
         showDatePicker()
+        setupData()
     }
-    
+
+    func setupData() {
+        guard let taskEdit = dataEdit else {
+            return
+        }
+        nameTaskTextField.text = taskEdit.title
+        descriptionTextField.text = taskEdit.description
+        isGroup = taskEdit.isGroup
+        isImportant = taskEdit.important
+        createButton.setTitle("Submit", for: .normal)
+        headerLabel.text = "Update task"
+    }
+
     override func viewDidAppear(_: Bool) {
-        setColorCategory(category: isPerson)
+        setColorCategory(category: isGroup)
         setImageImportant(isImportant: isImportant)
     }
-    
+
     private func setupTexField() {
         nameTaskTextField.delegate = self
         descriptionTextField.delegate = self
         dateTextField.delegate = self
         timeTextField.delegate = self
     }
-    
+
     private func showTimePicker() {
         timePicker.datePickerMode = .time
         timePicker.addTarget(self, action: #selector(timeChanger(timePicker:)), for: .valueChanged)
         timePicker.frame.size = CGSize(width: 0, height: 300)
         timePicker.preferredDatePickerStyle = .wheels
         timeTextField.inputView = timePicker
-        timeTextField.text = currentDate.formatTimeToString()
+        timePicker.locale = Locale(identifier: "en_GB")
+        timeTextField.text = dataEdit?.time ?? currentDate.formatTimeToString()
+        if let currentTimeText = timeTextField.text,
+           let currentTime = currentTimeText.toTime()
+        {
+            timePicker.date = currentTime
+        }
     }
-    
+
     @objc private func timeChanger(timePicker: UIDatePicker) {
         timeTextField.text = timePicker.date.formatTimeToString()
     }
-    
-    
-    
+
     private func showDatePicker() {
         datePicker.datePickerMode = .date
         datePicker.addTarget(self, action: #selector(dateChange(datePicker:)), for: .valueChanged)
         datePicker.frame.size = CGSize(width: 0, height: 300)
         datePicker.preferredDatePickerStyle = .wheels
         dateTextField.inputView = datePicker
-        dateTextField.text = currentDate.formatDateToString()
+        dateTextField.text = dataEdit?.date ?? currentDate.formatDateToString()
+        if let currentDateText = dateTextField.text,
+           let currentDate = currentDateText.toDate()
+        {
+            datePicker.date = currentDate
+        }
     }
-    
+
     @objc private func dateChange(datePicker: UIDatePicker) {
         dateTextField.text = datePicker.date.formatDateToString()
     }
-    
-    
-    
+
     private func setupLayout() {
-        self.view.layer.cornerRadius = 20
+        view.layer.cornerRadius = 20
         cancelButton.applyOutletButton(cornerRadius: 16, borderWidth: 1, borderColor: UIColor(hex: "#456ADD"))
         guard let dateImage = UIImage(named: "calender"),
               let timeImage = UIImage(named: "Time") else { return }
         dateTextField.addIconToLeft(image: dateImage, padding: 10)
         timeTextField.addIconToLeft(image: timeImage, padding: 10)
     }
-    
+
     @IBAction private func didTapUnCheckButton(_: Any) {
         isImportant = false
         setImageImportant(isImportant: isImportant)
     }
-    
+
     @IBAction private func didTapCheckButton(_: Any) {
         isImportant = true
         setImageImportant(isImportant: isImportant)
     }
-    
+
     private func setImageImportant(isImportant: Bool) {
         guard let check = UIImage(named: "check_done"),
               let unCheck = UIImage(named: "check") else { return }
@@ -128,40 +151,40 @@ class AddTaskViewController: UIViewController {
             notImportantButton.setImage(check, for: .normal)
         }
     }
-    
+
     @IBAction private func didTapPersonButton(_: Any) {
-        isPerson = true
-        setColorCategory(category: isPerson)
+        isGroup = false
+        setColorCategory(category: isGroup)
     }
-    
+
     @IBAction private func didTapGroupButton(_: Any) {
-        isPerson = false
-        setColorCategory(category: isPerson)
+        isGroup = true
+        setColorCategory(category: isGroup)
     }
-    
+
     private func setColorCategory(category: Bool) {
         guard let personFill = UIImage(systemName: "person.fill"),
               let person = UIImage(systemName: "person"),
               let groupFill = UIImage(systemName: "person.3.fill"),
               let group = UIImage(systemName: "person.3") else { return }
-        
         if category {
-            personButton.setImage(personFill, for: .normal)
-            personButton.tintColor = UIColor(hex: "#456ADD")
-            groupButton.setImage(group, for: .normal)
-            groupButton.tintColor = UIColor(hex: "#BFBFBF")
-        } else {
             personButton.setImage(person, for: .normal)
             personButton.tintColor = UIColor(hex: "#BFBFBF")
             groupButton.setImage(groupFill, for: .normal)
             groupButton.tintColor = UIColor(hex: "#456ADD")
+
+        } else {
+            personButton.setImage(personFill, for: .normal)
+            personButton.tintColor = UIColor(hex: "#456ADD")
+            groupButton.setImage(group, for: .normal)
+            groupButton.tintColor = UIColor(hex: "#BFBFBF")
         }
     }
-    
+
     @IBAction private func didTapCancelButton(_: Any) {
         dismiss(animated: true)
     }
-    
+
     @IBAction private func didTapCreateButton(_: Any) {
         guard let title = nameTaskTextField.text, !title.isEmpty,
               let description = descriptionTextField.text, !description.isEmpty,
@@ -171,27 +194,49 @@ class AddTaskViewController: UIViewController {
             showAlert(title: "Alert", message: "Please fill all field")
             return
         }
-        let newTask = TaskModel(id: "", title: title, description: description, important: isImportant, date: date, time: time, isGroup: isPerson)
-        addTask(newTask: newTask)
+
+        let taskID = dataEdit?.id ?? ""
+        let newTask = TaskModel(id: taskID, title: title, description: description, important: isImportant, date: date, time: time, isGroup: isGroup)
+        submitTask(newTask: newTask)
     }
-    
-    private func addTask(newTask: TaskModel){
+
+    private func submitTask(newTask: TaskModel) {
         guard let delegate = delegate else { return }
-        
-        taskService.addTask(task: newTask) { result in
+        if newTask.id.isEmpty {
+            addTask(newTask: newTask, delegate: delegate)
+        } else {
+            updateTask(newTask: newTask, delegate: delegate)
+        }
+    }
+
+    private func updateTask(newTask: TaskModel, delegate: AddTaskViewControllerDelegate) {
+        taskService.updateTask(task: newTask) { result in
             switch result {
-            case let .success(taskResponse):
-                delegate.didCreateTask(newTask: taskResponse)
+            case .success:
+                delegate.didUpdateTask()
                 self.dismiss(animated: true)
-                self.showAlert(title: "notification", message: "Add new task complete")
-                
+
             case let .failure(error):
                 print(error)
                 self.showAlert(title: "Error", message: "Some thing is wrong: \(error)")
             }
         }
     }
-    
+
+    private func addTask(newTask: TaskModel, delegate: AddTaskViewControllerDelegate) {
+        taskService.addTask(task: newTask) { result in
+            switch result {
+            case .success:
+                delegate.didCreateTask()
+                self.dismiss(animated: true)
+
+            case let .failure(error):
+                print(error)
+                self.showAlert(title: "Error", message: "Some thing is wrong: \(error)")
+            }
+        }
+    }
+
     private func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
@@ -199,12 +244,12 @@ class AddTaskViewController: UIViewController {
     }
 }
 
-extension AddTaskViewController: UITextFieldDelegate, UITextViewDelegate {
+extension AddUpdateTaskViewController: UITextFieldDelegate, UITextViewDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    
+
     override func touchesBegan(_: Set<UITouch>, with _: UIEvent?) {
         view.endEditing(true)
     }
